a9aab5ecf01471674a2687e051a22858d766d6c2
Soren Peterson <sopeters@open-230.mikeshouse.bravo>
2016-05-19 10:01:03 -0500

    simple visual automation

---
+++ 2/test.js
@@ -0,0 +1,54 @@
+var fs = require('fs');
+var base64 = require('base64-js');
+var resemble = require('resemble').resemble;
+var uuid = require('node-uuid');
+var Image = require('canvas').Image;
+
+var webdriver = require('selenium-webdriver'),
+    By = require('selenium-webdriver').By,
+    until = require('selenium-webdriver').until;
+
+function save (data) {
+  return new Promise(function (resolve, reject) {
+    filename = 'images/' + uuid.v1() + '.png';
+    fs.writeFile(filename, data, 'base64', function (err) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(filename);
+      }
+    })
+  });
+}
+
+function createImage (url) {
+  var image = new Image();
+  image.src = url;
+  return image;
+}
+
+function getScreenshot (browser) {
+  var driver = new webdriver.Builder()
+      .forBrowser(browser)
+      .build();
+
+  driver.get('http://www.google.com/ncr');
+  return new Promise(function (resolve, reject) {
+    driver.takeScreenshot().then(function (data) {
+      save(data).then(function (filename) {
+        resolve(filename);
+      });
+    });
+    driver.quit();
+  })
+}
+
+Promise.all([getScreenshot('chrome'), getScreenshot('firefox')]).then(function (results) {
+  console.log(results);
+  debugger;
+  resemble(results[0]).compareTo(results[1]).onComplete(function (data) {
+    fs.writeFile('result.png', data.getBuffer(), 'base64', function () {
+      console.log(arguments);
+    });
+  })
+})
